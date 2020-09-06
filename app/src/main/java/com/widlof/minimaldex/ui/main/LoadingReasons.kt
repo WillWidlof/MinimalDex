5d5d56eda50705ca12c2ccd749442670f1d9dd75
WillWidlof <widlof7239@gmail.com>
2020-09-06 20:37:52 +0100

    Added more Pokemon themed loading reasons for the loading bar easter egg

--- app/src/main/java/com/widlof/minimaldex/ui/main/LoadingReasons.kt
+++ app/src/main/java/com/widlof/minimaldex/ui/main/LoadingReasons.kt
@@ -3,7 +3,11 @@ package com.widlof.minimaldex.ui.main
 enum class LoadingReasons(val reason: String) {
     CATCHING_POKEMON("Catching Pokemon..."),
     POKEMON_CENTRE("Visiting the Pokemon Centre..."),
-    TRADING_POKEMON("Trading Pokemon"),
-    GYM_LEADER("Fighting the Gym Leader"),
-    PROFESSOR_OAK("Visiting Professor Oak")
+    TRADING_POKEMON("Trading Pokemon..."),
+    GYM_LEADER("Fighting the Gym Leader..."),
+    PROFESSOR_OAK("Visiting Professor Oak..."),
+    POKEMON_LEAGUE("Taking on the Pokemon League..."),
+    TEAM_ROCKET("Preparing for trouble..."),
+    POKEMON_DAY_CARE("Visiting the Pokemon Day Care Centre..."),
+    PALLET_TOWN("Heading to Pallet Town...")
 }
